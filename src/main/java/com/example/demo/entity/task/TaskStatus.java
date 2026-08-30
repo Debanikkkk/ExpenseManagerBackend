5bb72f1c12c2d75f04b78be5df17a7c943d042ec
Debanik Pahilwani <debanik@Debaniks-MacBook-Pro.local>
2026-08-30 14:10:30 +0530

    changes made to task service and repo

--- src/main/java/com/example/demo/entity/task/TaskStatus.java
+++ src/main/java/com/example/demo/entity/task/TaskStatus.java
@@ -1,5 +1,8 @@
 package com.example.demo.entity.task;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum TaskStatus {
     TODO("Todo"),
     IN_PROGRESS("In Progress"),
@@ -11,7 +14,18 @@ public enum TaskStatus {
         this.label = label;
     }
 
+    @JsonValue
     public String getLabel() {
         return label;
+    }
+
+    @JsonCreator
+    public static TaskStatus fromValue(String value) {
+        for (TaskStatus status : values()) {
+            if (status.label.equalsIgnoreCase(value) || status.name().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unknown TaskStatus: " + value);
     }
 }
